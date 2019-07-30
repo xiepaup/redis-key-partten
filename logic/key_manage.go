78b19354d93c61e71f2a43b3e0049803906dc322
xiepaup <xiepaup@163.com>
2019-07-30 16:24:22 +0800

    fix some bugs

--- logic/key_manage.go
+++ logic/key_manage.go
@@ -30,25 +30,27 @@ type RegItem struct {
 	Reg  *regexp.Regexp
 }
 
-func NewGuessKeyMode(samples, confidence int64, sample int, delimiter string) *GuessKeyMode {
+func NewGuessKeyMode(samples, confidence, adjfactor int64, sample int, delimiter string) *GuessKeyMode {
 	return &GuessKeyMode{
 		MaxGusessCnt:    samples,
 		SplitDelimiter:  delimiter,
 		WrodsConfidence: confidence,
 		MaxSampleList:   sample,
 		StatsTotal:      0,
-		AdjustFactor:    4,
+		AdjustFactor:    adjfactor,
 		GuseKeySampleEntry: &GuseKeySample{
-			KeySamples:       map[string]struct{}{},
-			TmpWordsSample:   map[string]int64{},
-			KnowedSmartWrods: map[string]struct{}{},
+			KeySamples:       map[string]int{},
+			TmpWordsSample:   map[string]*SmartWords{},
+			KnowedSmartWrods: map[string]int64{},
 		},
 		StatKeyPartten: map[string]*KeyPartten{},
 		regRule: []*RegItem{&RegItem{Mode: "[0-9]+", Reg: regexp.MustCompile(`^[[:digit:]]+$`)},
 			&RegItem{Mode: "[A-Za-z]+", Reg: regexp.MustCompile(`^[[:alpha:]]+$`)},
-			&RegItem{Mode: "[0-9A-Za-z]+", Reg: regexp.MustCompile(`[[:alnum:]]+`)},
-			&RegItem{Mode: "[0-9A-Za-z_]+", Reg: regexp.MustCompile(`[[:word:]]+`)}},
-		SepcialRule: []*RegItem{&RegItem{Mode: "[YYYMMDD]", Reg: regexp.MustCompile(`^20[0-9]{2}[0|1][0-9][0-3][0-9]$`)},
+			&RegItem{Mode: "[A-Za-z_0-9]+", Reg: regexp.MustCompile(`[[:alnum:]]+`)},
+			&RegItem{Mode: "[.*]+", Reg: regexp.MustCompile(`.+`)}},
+		SepcialRule: []*RegItem{
+			&RegItem{Mode: "[YYYMMDD]", Reg: regexp.MustCompile(`^[D|d]*20[0-9]{2}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$`)},
+			//&RegItem{Mode: "[YYYMMDD]", Reg: regexp.MustCompile(`^20[0-9]{2}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$`)},
 			&RegItem{Mode: "[YYYMM]", Reg: regexp.MustCompile(`^20[0-9]{2}[0|1][0-9]$`)},
 			&RegItem{Mode: "[date_long]", Reg: regexp.MustCompile(`^15[0-9]{8,10}$`)}},
 		DelimiterAuto: []string{"|", "_", ":", "#"},
@@ -56,10 +58,15 @@ func NewGuessKeyMode(samples, confidence int64, sample int, delimiter string) *G
 }
 
 type GuseKeySample struct {
-	KeySamples       map[string]struct{}
-	KnowedSmartWrods map[string]struct{}
-	TmpWordsSample   map[string]int64
+	KeySamples       map[string]int
+	KnowedSmartWrods map[string]int64
+	TmpWordsSample   map[string]*SmartWords
 	GuseCnt          int64
+}
+
+type SmartWords struct {
+	Cnt  int64
+	Used int64
 }
 
 type KeyPartten struct {
@@ -73,18 +80,37 @@ type KeyPartten struct {
 func (this *GuessKeyMode) Run(ctx context.Context, keyCh chan string) {
 	for {
 		select {
-		case <-ctx.Done():
-			return
-		case k := <-keyCh:
+		case k, ok := <-keyCh:
+			if !ok {
+				return
+			}
 			this.StatsTotal++
 			if ok := this.WordsLearning(k); ok {
 				this.GuessKeyPartten(false)
+				bx, _ := json.Marshal(this.GuseKeySampleEntry)
+				//log.Debugf("I'am Done :%d ||| GuesKeyInfo :%s  ||| StatKeyPartten : %s", this.StatsTotal, bx, by)
+				log.Debugf("I'am Done :%d ||| GuesKeyInfo :%s ", this.StatsTotal, bx)
 			}
-			if (this.StatsTotal % 500000) == 0 {
+			if (this.StatsTotal % 200000) == 0 {
+				//this.AdjustFactor++
+				this.WrodsConfidence++
+			}
+			if (this.StatsTotal % 1000000) == 0 {
 				this.AdjustFactor++
 			}
+			//case <-ctx.Done():
+			//	//return
+			//	goto TAG_END_LOOP
 		}
 	}
+}
+
+func (this *GuessKeyMode) SetKnownSmartsWords(w string) {
+	err := json.Unmarshal([]byte(w), &this.GuseKeySampleEntry.KnowedSmartWrods)
+	if err != nil {
+		log.Warnf("load known smart words failed : %v", err)
+	}
+
 }
 func (this *GuessKeyMode) GuessDelimiter(k string) string {
 	var max, firstIdx int
@@ -92,7 +118,6 @@ func (this *GuessKeyMode) GuessDelimiter(k string) string {
 	firstIdx = 999999
 	for _, d := range this.DelimiterAuto {
 		ws := strings.Split(k, d)
-    //NOTE(xiepaup) here for have equal delimiter ; add privilege for delimiter
 		t_fristIdx := len(ws[0])
 		log.Debugf("for k : %s ; delimiter : %s ; args : splen :curmax:%d %d,fidx: %d", k, d, max, len(ws), t_fristIdx)
 		if len(ws) >= max && t_fristIdx < firstIdx {
@@ -113,7 +138,8 @@ func (this *GuessKeyMode) AutoDelimiter(k string) ([]string, string) {
 		this.TrueSplitDelimiter = this.SplitDelimiter
 	}
 	words := strings.Split(k, this.TrueSplitDelimiter)
-	return words, fmt.Sprintf("%d%s%d", len(words), this.TrueSplitDelimiter, len(words))
+	//return words, fmt.Sprintf("%d%s%d", len(words), this.TrueSplitDelimiter, len(words))
+	return words, ""
 }
 
 func (this *GuessKeyMode) WordsLearning(s string) bool {
@@ -125,21 +151,30 @@ func (this *GuessKeyMode) WordsLearning(s string) bool {
 			//use less memory
 			for _, reg := range this.SepcialRule {
 				if reg.Reg.MatchString(w) {
-					//ts = strings.Replace(s, w, reg.Mode, 1)
 					w = reg.Mode
 					break
 				}
 			}
-			this.GuseKeySampleEntry.TmpWordsSample[w]++
+			if _, ok := this.GuseKeySampleEntry.KnowedSmartWrods[w]; !ok {
+				if _, ok := this.GuseKeySampleEntry.TmpWordsSample[w]; !ok {
+					//log.Infof("kkk:%s: %v : %v", w, ok, this.GuseKeySampleEntry.KnowedSmartWrods[w])
+					this.GuseKeySampleEntry.TmpWordsSample[w] = &SmartWords{Cnt: 1, Used: 0}
+				} else {
+					//here no add!!
+					this.GuseKeySampleEntry.TmpWordsSample[w].Cnt++
+				}
+			}
 		}
-		this.GuseKeySampleEntry.KeySamples[s] = struct{}{}
+		this.GuseKeySampleEntry.KeySamples[s] = 0
+		//b, _ := json.Marshal(this.GuseKeySampleEntry.TmpWordsSample)
+		//log.Infof("after key : %s , words : %s", s, b)
 		return false
 	}
 
 	//recaculate words konwing
 	log.Debugf("words konwing : %#v", this.GuseKeySampleEntry.TmpWordsSample)
 	for w, c := range this.GuseKeySampleEntry.TmpWordsSample {
-		if c < this.AdjustFactor {
+		if c.Cnt < this.AdjustFactor && c.Used <= 0 {
 			delete(this.GuseKeySampleEntry.TmpWordsSample, w)
 		}
 	}
@@ -151,10 +186,7 @@ func (this *GuessKeyMode) WordsLearning(s string) bool {
 
 //called by locked_func
 func (this *GuessKeyMode) GetStringModle(w string) (tmp_partten string, transfered bool) {
-	//b, _ := json.Marshal(this.GuseKeySampleEntry[gks].TmpWordsSample)
-	//log.Debugf("<<<<<<<todo key sample is : %s", b)
-
-	//NOTE(xiepaup) for learning history , in history ; think as good
+	//NOTE(vitoxie) for learning history , in history ; think as good
 	if _, ok := this.GuseKeySampleEntry.KnowedSmartWrods[w]; ok {
 		return w, true
 	}
@@ -176,7 +208,8 @@ func (this *GuessKeyMode) GetStringModle(w string) (tmp_partten string, transfer
 			}
 		}
 	} else {
-		if this.GuseKeySampleEntry.TmpWordsSample[w] > this.WrodsConfidence {
+		if this.GuseKeySampleEntry.TmpWordsSample[w].Cnt > this.WrodsConfidence {
+			this.GuseKeySampleEntry.TmpWordsSample[w].Used++
 			tmp_partten = w
 			transfered = true
 		} else {
@@ -192,7 +225,6 @@ func (this *GuessKeyMode) GuessKeyPartten(finall bool) {
 	for k, _ := range this.GuseKeySampleEntry.KeySamples {
 		//words := strings.Split(k, this.SplitDelimiter)
 		words, _ := this.AutoDelimiter(k)
-		//gks := fmt.Sprintf("%d%s%d", len(words), this.SplitDelimiter, len(words))
 		var tmp_partten string
 		var transfered bool
 		for _, w := range words {
@@ -209,54 +241,61 @@ func (this *GuessKeyMode) GuessKeyPartten(finall bool) {
 			}
 			tmp_partten = fmt.Sprintf("%s%s%s", tmp_partten, this.TrueSplitDelimiter, ttp)
 		}
+		//for 6578ccee9cad04c7b011ddb1ee7e6f0c these keys , cloud no transfer!!! any more
+		if len(words) == 1 || this.GuseKeySampleEntry.KeySamples[k] >= 3 {
+			transfered = true
+			tmp_partten = fmt.Sprintf("%s%s", this.TrueSplitDelimiter, "[.*]+")
+		}
 		log.Debugf("key : %s :: partten : %s", k, tmp_partten)
 		if !transfered && !finall {
 			repushwords = true
 			//here should do recaculate smart words !
 			for _, w := range words {
-				this.GuseKeySampleEntry.TmpWordsSample[w]++
+				if _, ok := this.GuseKeySampleEntry.TmpWordsSample[w]; !ok {
+					this.GuseKeySampleEntry.TmpWordsSample[w] = &SmartWords{Cnt: 1, Used: 0}
+				}
 			}
+			this.GuseKeySampleEntry.KeySamples[k]++
 			continue
 		} else {
 			if finall && !transfered {
-				tmp_partten = k
+				tmp_partten = "[.*]+"
 			} else {
 				tmp_partten = tmp_partten[len(this.TrueSplitDelimiter):]
 			}
 		}
-		//finall partten for key : 9305:status:471721393834893049 : :9403:rankforbid:[0-9a-z]
+		//finall partten for key : 9305:status:471721049 : :9403:rankforbid:[0-9a-z]
 		log.Debugf("====>finall partten for key : %s : %s", k, tmp_partten)
 
 		delete(this.GuseKeySampleEntry.KeySamples, k)
 		if _, ok := this.StatKeyPartten[tmp_partten]; !ok {
-			this.StatKeyPartten[tmp_partten] = &KeyPartten{KeySamples: []string{}}
+			this.StatKeyPartten[tmp_partten] = &KeyPartten{KeySamples: []string{}, KeyModes: tmp_partten}
 		}
 		if this.MaxSampleList > len(this.StatKeyPartten[tmp_partten].KeySamples) {
 			this.StatKeyPartten[tmp_partten].KeySamples = append(this.StatKeyPartten[tmp_partten].KeySamples, k)
 		}
 		this.StatKeyPartten[tmp_partten].KeyTotalCnt++
-		this.StatKeyPartten[tmp_partten].KeyModes = tmp_partten
+		//this.StatKeyPartten[tmp_partten].KeyModes = tmp_partten
 	}
 	this.GuseKeySampleEntry.GuseCnt = int64(len(this.GuseKeySampleEntry.KeySamples))
-	bb, _ := json.Marshal(this.StatKeyPartten)
 	if repushwords {
 		for w, c := range this.GuseKeySampleEntry.TmpWordsSample {
-			if c < 2 {
+			if c.Cnt < this.AdjustFactor && c.Used <= 0 {
 				delete(this.GuseKeySampleEntry.TmpWordsSample, w)
 			}
 		}
 	}
-	//mutex.Unlock()
-	log.Debugf("Neweast key sample is : %s", bb)
-	bb, _ = json.Marshal(this.GuseKeySampleEntry.TmpWordsSample)
-	log.Debugf("===>>smart words : %s", bb)
+	//bb, _ := json.Marshal(this.StatKeyPartten)
+	//log.Debugf("Neweast key sample is : %s", bb)
+	//bb, _ = json.Marshal(this.GuseKeySampleEntry.TmpWordsSample)
+	//log.Debugf("===>>smart words : %s", bb)
 }
 
 func (this *GuessKeyMode) GetSampledKeys() map[string]*KeyPartten {
 	b, _ := json.Marshal(this.GuseKeySampleEntry)
 	log.Debugf("do recaculate smart words : %s", b)
 	for w, c := range this.GuseKeySampleEntry.TmpWordsSample {
-		if c < this.AdjustFactor {
+		if c.Cnt < this.AdjustFactor && c.Used <= 0 {
 			delete(this.GuseKeySampleEntry.TmpWordsSample, w)
 		}
 	}
@@ -266,7 +305,7 @@ func (this *GuessKeyMode) GetSampledKeys() map[string]*KeyPartten {
 	for kp, _ := range this.StatKeyPartten {
 		if this.StatKeyPartten[kp].KeyTotalCnt < 2 {
 			for _, k := range this.StatKeyPartten[kp].KeySamples {
-				this.GuseKeySampleEntry.KeySamples[k] = struct{}{}
+				this.GuseKeySampleEntry.KeySamples[k] = 0
 			}
 			delete(this.StatKeyPartten, kp)
 		}
@@ -274,13 +313,14 @@ func (this *GuessKeyMode) GetSampledKeys() map[string]*KeyPartten {
 	return this.StatKeyPartten
 }
 
-func (this *GuessKeyMode) GetSmartWords() map[string]int64 {
-	words := map[string]int64{}
+func (this *GuessKeyMode) GetSmartWords() map[string]*SmartWords {
+	words := map[string]*SmartWords{}
 	for k, c := range this.GuseKeySampleEntry.TmpWordsSample {
 		if _, ok := words[k]; !ok {
-			words[k] = c
+			words[k] = &SmartWords{Cnt: c.Cnt, Used: c.Used}
 		} else {
-			words[k] += c
+			words[k].Cnt += c.Cnt
+			words[k].Used += c.Used
 		}
 	}
 	return words
@@ -291,7 +331,7 @@ func (this *GuessKeyMode) ShowSampledKeys() {
 	for f, kpp := range this.StatKeyPartten {
 		i++
 		b, _ := json.Marshal(kpp)
-		log.Infof("%5d %s Parttend : %s", i, f, b)
+		log.Infof("%5d %s : %s", i, f, b)
 	}
 }
 
@@ -306,7 +346,7 @@ func (this *GuessKeyMode) PolymerizationSampledKeys(sks ...map[string]*KeyPartte
 			this.StatKeyPartten[f].keyTotalFileds += kpp.keyTotalFileds
 			this.StatKeyPartten[f].keyTotalValSize += kpp.keyTotalValSize
 			for _, s := range kpp.KeySamples {
-				if len(this.StatKeyPartten[f].KeySamples) > this.MaxSampleList {
+				if len(this.StatKeyPartten[f].KeySamples) >= this.MaxSampleList {
 					break
 				}
 				this.StatKeyPartten[f].KeySamples = append(this.StatKeyPartten[f].KeySamples, s)
@@ -315,16 +355,18 @@ func (this *GuessKeyMode) PolymerizationSampledKeys(sks ...map[string]*KeyPartte
 	}
 }
 
-func (this *GuessKeyMode) PolymerizationSmartWords(smarts ...map[string]int64) map[string]int64 {
-	words := map[string]int64{}
+func (this *GuessKeyMode) PolymerizationSmartWords(smarts ...map[string]*SmartWords) map[string]int64 {
+	//words := map[string]int64{}
+
+	smarts = append(smarts, this.GuseKeySampleEntry.TmpWordsSample)
 	for _, smart := range smarts {
 		for k, c := range smart {
-			if _, ok := words[k]; !ok {
-				words[k] = c
+			if _, ok := this.GuseKeySampleEntry.KnowedSmartWrods[k]; !ok {
+				this.GuseKeySampleEntry.KnowedSmartWrods[k] = c.Cnt
 			} else {
-				words[k] += c
+				this.GuseKeySampleEntry.KnowedSmartWrods[k] += c.Cnt
 			}
 		}
 	}
-	return words
+	return this.GuseKeySampleEntry.KnowedSmartWrods
 }
